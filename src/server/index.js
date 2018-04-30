0a792dc0a4291a7f733501b4edaa923022214ad0
Jay Lynch <hi@jaylyn.ch>
2018-05-01 00:52:44 +1000

    Update page title

--- src/server/index.js
+++ src/server/index.js
@@ -46,7 +46,7 @@ server
               <head>
                   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
                   <meta charset="utf-8" />
-                  <title>Welcome to Razzle</title>
+                  <title>Isegoria</title>
                   <meta name="viewport" content="width=device-width, initial-scale=1">
                   ${assets.client.css
                     ? `<link rel="stylesheet" href="${assets.client.css}">`
