edcf5d6cff8f36fec61a3549ec35215e065c8320
Jay Lynch <hi@jaylyn.ch>
2018-10-15 02:20:15 +1100

    Auto-login if local credentials available

--- src/client/index.js
+++ src/client/index.js
@@ -6,11 +6,13 @@ import { ConnectedRouter } from 'connected-react-router';
 import { createBrowserHistory } from 'history';
 import configureStore from '../common/store/configureStore';
 
+import * as AuthActions from '../common/actions/auth';
+
 const history = createBrowserHistory();
 const initialState = window.__INITIAL_STATE__;
 const initialAuth = initialState.auth;
 
-const localAuth = window && window.localStorage
+const localAuth = (window && window.localStorage)
   ? { user: JSON.parse(window.localStorage.getItem('u')) }
   : {}
 
@@ -24,15 +26,30 @@ const store = configureStore(Object.assign(
   }
 ), history);
 
-let currentState;
+let currentState = {
+  auth: {}
+};
 const unsubscribeUser = store.subscribe(() => {
   let prevState = currentState;
   currentState = store.getState();
 
-  if (window && window.localStorage) {
-    window.localStorage.setItem('u', JSON.stringify(currentState.auth.user));
+  const userChanged = currentState.auth.user !== prevState.auth.user
+  const hasPassword = !!currentState.auth.user.password
+  const localStorageAvailable = window && window.localStorage
+
+  if (localStorageAvailable && userChanged && hasPassword) {
+    window.localStorage.setItem(
+      'u', JSON.stringify(currentState.auth.user)
+    );
   }
 })
+
+if (localAuth.user && localAuth.user.password) {
+  const { email, password } = localAuth.user
+  store.dispatch(
+    AuthActions.attemptLogin(email, password)
+  );
+}
 
 hydrate(
   <Provider store={store}>
