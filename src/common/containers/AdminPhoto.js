8a496efdbc068e3a49f77b4ec7c9f86967f26411
Vivien <hi@vivienxu.com>
2019-03-07 20:49:16 +1100

    Add Add Photo button

--- src/common/containers/AdminPhoto.js
+++ src/common/containers/AdminPhoto.js
@@ -1,8 +1,24 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 import ContentItem from '../components/ContentItem';
+
+import Fab from '@material-ui/core/Fab';
+
+import AddIcon from '@material-ui/icons/Add';
+
+export const StyledAddIcon = styled(AddIcon)`
+  margin-right: 8px;
+`;
+
+export const StyledFab = styled(Fab)`
+  && {
+    bottom: 16px;
+    position: fixed;
+    right: 16px;
+  }
+`;
 
 class AdminPhoto extends React.Component {
   render() {
@@ -15,14 +31,30 @@ class AdminPhoto extends React.Component {
       ? photos.filter(photo => `${photo.nodeId}` === id)
       : photos;
 
-    return (
-      displayPhotos.map(photo =>
-        <ContentItem
-          contentType="photo"
-          item={photo}
-          key={`c-photos-photo-${photo.nodeId}`} />
-      )
-    );
+    return ([
+      <div>
+      {
+        displayPhotos.map(photo =>
+          <ContentItem
+            contentType="photo"
+            item={photo}
+            key={`c-photos-photo-${photo.nodeId}`} />
+        )
+      }
+      </div>,
+      <StyledFab
+        aria-label="Add"
+        color="primary"
+        key="add-photo"
+        onClick={
+          () => window.location.href += '/create'
+        }
+        variant="extended"
+      >
+        <StyledAddIcon />
+        Add Photo
+      </StyledFab>
+    ]);
   }
 }
 
